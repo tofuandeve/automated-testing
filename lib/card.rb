6c2738e45ad21aa36849fad7cba4d4cc49d6d19b
Eve Le <nguyenthao.lee@gmail.com>
2019-08-27 23:41:56 -0700

    Added tests and implementation for Card class

--- lib/card.rb
+++ lib/card.rb
@@ -1,18 +1,41 @@
-
-# card.rb
-
 class Card
+  VALUES = {
+    "1"=> "Ace",
+    "2"=> "2",
+    "3"=> "3",
+    "4"=> "4",
+    "5"=> "5",
+    "6"=> "6",
+    "7"=> "7",
+    "8"=> "8",
+    "9"=> "9",
+    "10"=> "10",
+    "11"=> "Jack",
+    "12"=> "Queen",
+    "13"=> "King"
+  }
+  
+  SUITS = [:hearts, :spades, :clubs, :diamonds]
   attr_reader :value, :suit
-
+  
   def initialize(value, suit)
+    raise ArgumentError.new("Invalid card value!") if !VALUES.keys.include?(value.to_s)
     @value = value
+    raise ArgumentError.new("Invalid card suit!") if !SUITS.include?(suit)
     @suit = suit
-    
-
   end
-
+  
   def to_s
-    return "#{value} of #{suit.to_s}"
+    return "#{VALUES[value.to_s]} of #{suit.to_s}"
   end
-
+  
+  def self.create_new_deck
+    cards = Array.new
+    VALUES.each do |key, value|
+      4.times do |index|
+        cards << Card.new(key.to_i, SUITS[index])
+      end
+    end
+    return cards
+  end
 end
--- test/card_test.rb
+++ test/card_test.rb
@@ -2,7 +2,7 @@ require_relative 'test_helper'
 
 describe Card do
   describe "You can create a Card instance" do
-
+    
     it "Can be created" do
       [:hearts, :spades, :clubs, :diamonds].each do |suit|
         (1..13).each do |value|
@@ -11,7 +11,7 @@ describe Card do
         end
       end
     end
-
+    
     it "Raises an ArgumentError for invalid parameters" do
       # To raise an ArgumentError in card.rb
       # raise ArgumentError.new
@@ -20,33 +20,87 @@ describe Card do
       expect {Card.new(14, :clovers)}.must_raise ArgumentError
     end
   end
-
+  
   describe "Testing to_s" do
-
     it "to_s returns a readable String value logically for values 2-10" do
-      # Test to ensure that to_s works for cards values 2-10
-      # for example:  "2 of diamonds"
+      (2..10).each do |value|
+        card = Card.new(value, :diamonds)
+        expect (card.to_s).must_equal "#{value} of diamonds"
+      end
     end
-
+    
     it "to_s returns a readable String value for Ace, Jack, Queen, King" do
-      # Test to ensure that to_s works for cards values 1, and 11-13
-      # For example: "Queen of hearts"
-      # The current implementation of to_s does not address this feature
-      # Write the test, see it fail, then modify to_s to make it pass!
-      # (Consider writing a helper method!)
+      face_cards = {
+        "1"=> "Ace",
+        "11"=> "Jack",
+        "12"=> "Queen",
+        "13"=> "King"
+      }
+      
+      (1..13).each do |value|
+        card = Card.new(value, :hearts)
+        if face_cards.keys.include? value.to_s
+          expect (card.to_s).must_equal "#{face_cards[value.to_s]} of hearts"
+        else
+          expect (card.to_s).must_equal "#{value} of hearts"
+        end
+      end
     end
   end
-
+  
   describe "Reader methods" do
-
     it "Can retrieve the value of the card using a `.value`." do
-      # ensure that `.value works as expected`
+      (1..13).each do |value|
+        card = Card.new(value, :hearts)
+        
+        expect (card.value).must_be_instance_of Integer
+        expect (card.value).must_equal value
+      end
     end
-
+    
     it "Can retrieve the value of the card using a `.suit`." do
-      # ensure that `.suit works as expected returning the symbol of the suit`
-
+      suits = [:hearts, :spades, :clubs, :diamonds]
+      suits.each do |suit|
+        card = Card.new(2, suit)
+        
+        expect (card.suit).must_be_instance_of Symbol
+        expect (card.suit).must_equal suit
+      end    
     end
   end
-
+  
+  describe "#self.create_new_deck method" do
+    NUMBER_OF_CARDS = 52
+    CARDS = Card.create_new_deck
+    VALID_SUITS = [:hearts, :spades, :clubs, :diamonds]
+    it "returns a deck of 52 cards" do
+      expect (CARDS).must_be_instance_of Array
+      expect (CARDS.length).must_equal NUMBER_OF_CARDS
+    end
+    
+    it "returns a deck of Card objects" do
+      NUMBER_OF_CARDS.times do |index|
+        expect (CARDS[index]).must_be_instance_of Card
+      end
+    end
+    
+    it "generates 4 cards for each value" do
+      my_value = 2
+      my_cards = CARDS.select { |card| card.value == my_value}
+      expect (my_cards.length).must_equal 4
+    end
+    
+    it "generates 4 different suits for each value" do
+      my_value = 11
+      my_cards = CARDS.select { |card| card.value == my_value}
+      expect (my_cards.length).must_equal 4
+      
+      my_suits = my_cards.map { |card| card.suit }
+      expect (my_suits.length).must_equal 4
+      assert_nil(my_suits.uniq!)
+      my_suits.each do |suit|
+        expect (VALID_SUITS.include?(suit)).must_equal true
+      end
+    end       
+  end     
 end
